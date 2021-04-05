1f7b4976cd11fcc1f1fce9274b61ecc501d54eb7
brerickner <breannarickner@gmail.com>
2021-04-05 01:09:44 +0000

    updating config

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 
 file_line { 'Turn off passwd auth':
     ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
+    path   => '/ssh/config',
     line   => '    PasswordAuthentication no',
 }
 
 file_line {'Declare identity file':
     ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
+    path   => '/ssh/config',
     line   => '    IdentityFile ~/.ssh/holberton',
 }
