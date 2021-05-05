43fb2c9e36d77b3261d072c640855af76db4134b
brerickner <breannarickner@gmail.com>
2021-05-05 19:03:54 +0000

    adding task 3. pepfixed. output good

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -19,7 +19,6 @@ if __name__ == "__main__":
         emp_id_list.append(emp_id)
 
         for employee in emp_id_list:
-            emp_id = employee
             dict_list = []
             final_list = []
             the_file = "todo_all_employees.json"
@@ -27,6 +26,7 @@ if __name__ == "__main__":
             to_dos = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
                 .format(emp_id)
             todo_resp = requests.get(to_dos)
+
             for data in todo_resp.json():
                 task = data['title']
                 done = data['completed']
@@ -34,8 +34,8 @@ if __name__ == "__main__":
                     "task": task, "completed": done, "username": u_name
                 }
                 dict_list.append(task_dict)
-                final_dict[emp_id] = dict_list
 
-                with open(the_file, mode='a') as json_file:
-                    j_str = json.dump(final_dict)
-                    json_file.write(j_str)
+            final_dict[emp_id] = dict_list
+
+            with open(the_file, mode='a') as json_file:
+                json.dump(final_dict, json_file)
