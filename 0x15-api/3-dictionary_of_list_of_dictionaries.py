028917e8ffe21d56ed29d616e7d86d34225e621e
brerickner <breannarickner@gmail.com>
2021-05-05 19:51:03 +0000

    come onnnnnnnnnnnnnn

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -21,20 +21,17 @@ if __name__ == "__main__":
         final_dict = {}
         final_dict[user_id] = []
 
-        for employee in user_id_list:
-            the_file = "todo_all_employees.json"
-            to_dos = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
-                .format(user_id)
-            todo_resp = requests.get(to_dos)
-
-            for data in todo_resp.json():
-                task = data["title"]
-                done = data["completed"]
-                task_dict = {
-                    "task": task, "completed": done, "username": u_name
-                }
-                final_dict.get(user_id).append(task_dict)
-
-                with open(the_file, mode='w') as json_file:
-                    j_str = json.dumps(final_dict)
-                    json_file.write(j_str)
+        the_file = "todo_all_employees.json"
+        to_dos = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
+            .format(user_id)
+        todo_resp = requests.get(to_dos)
+        for data in todo_resp.json():
+            task = data["title"]
+            done = data["completed"]
+            task_dict = {
+                "task": task, "completed": done, "username": u_name
+            }
+            final_dict.get(user_id).append(task_dict)
+            with open(the_file, mode='w') as json_file:
+                j_str = json.dumps(final_dict)
+                json_file.write(j_str)
