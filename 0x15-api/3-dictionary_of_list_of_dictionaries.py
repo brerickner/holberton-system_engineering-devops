15141d23eccb221cd7cacc564068016a49231f76
brerickner <breannarickner@gmail.com>
2021-05-05 18:34:26 +0000

    adding progress for 3. output good. pepdfixed

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''Module with script to use REST API to track employee ID
+and return todo list  export data in the JSON format'''
+
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_name = 'https://jsonplaceholder.typicode.com/users/'
+    user_resp = requests.get(user_name).json()
+    emp_id_list = []
+    for emps in user_resp:
+        emp_name = emps['name']
+        u_name = emps['username']
+        emp_id = emps['id']
+        emp_id_list.append(emp_id)
+
+        for employee in emp_id_list:
+            emp_id = employee
+            dict_list = []
+            final_list = []
+            the_file = "todo_all_employees.json"
+            final_dict = {}
+            to_dos = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(emp_id)
+            todo_resp = requests.get(to_dos)
+            for data in todo_resp.json():
+                task = data['title']
+                task_dict = {
+                    "task": task, "completed": done, "username": u_name
+                }
+                dict_list.append(task_dict)
+                final_dict[emp_id] = dict_list
+
+                with open(the_file, mode='a') as json_file:
+                    j_str = json.dumps(final_dict)
+                    json_file.write(j_str)
