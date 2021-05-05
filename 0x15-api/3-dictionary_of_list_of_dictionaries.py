426f1bd45c52f62312103e197a72e886fa4df5e0
brerickner <breannarickner@gmail.com>
2021-05-05 19:39:14 +0000

    come one this is ridiculous

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,31 +11,31 @@ import sys
 if __name__ == "__main__":
     user_name = 'https://jsonplaceholder.typicode.com/users/'
     user_resp = requests.get(user_name).json()
-    emp_id_list = []
-    for emps in user_resp:
-        emp_name = emps.get('name')
-        u_name = emps.get('username')
-        emp_id = emps.get('id')
-        emp_id_list.append(emp_id)
+    user_id_list = []
 
-        for employee in emp_id_list:
-            dict_list = []
-            final_list = []
+    for emps in user_resp:
+        emp_name = emps.get("name")
+        u_name = emps.get("username")
+        user_id = emps.get("id")
+        user_id_list.append(user_id)
+        final_dict = {}
+        final_dict[user_id] = []
+
+        for employee in user_id_list:
             the_file = "todo_all_employees.json"
-            final_dict = {}
             to_dos = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
-                .format(emp_id)
+                .format(user_id)
             todo_resp = requests.get(to_dos)
 
             for data in todo_resp.json():
-                task = data['title']
-                done = data['completed']
+                task = data["title"]
+                done = data["completed"]
                 task_dict = {
                     "task": task, "completed": done, "username": u_name
                 }
-                dict_list.append(task_dict)
+                final_dict.get(user_id).append(task_dict)
+            print(final_dict)
 
-            final_dict[emp_id] = dict_list
-
-            with open(the_file, mode='a') as json_file:
-                json.dump(final_dict, json_file)
+            with open(the_file, mode='w') as json_file:
+                j_str = json.dumps(final_dict)
+                json_file.write(j_str)
