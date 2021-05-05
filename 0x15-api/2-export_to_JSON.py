8de134d979160ca83c066c43afb15cec55d96c4d
brerickner <breannarickner@gmail.com>
2021-05-05 14:38:05 +0000

    adding 2. pepfixed. output good

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -20,22 +20,17 @@ if __name__ == "__main__":
     tc = 0
     chk = 0
     task_list = []
-    task_dict = {"task": [], "completed": [], "username": u_name}
     the_file = "{}.json".format(emp_id)
     tasks_done = {}
     for data in todo_resp.json():
         done = data['completed']
         task = data['title']
-        task_dict["task"].append(task)
-        task_dict["completed"].append(done)
+        task_dict = {
+            "task": task, "completed": done, "username": u_name
+        }
         task_list.append(task_dict)
         tasks_done[emp_id] = task_list
 
         with open(the_file, mode='w') as json_file:
             j_str = json.dumps(tasks_done)
             json_file.write(j_str)
-    '''
-    # with open(the_file, mode='a') as csv_file:
-    #emp_tasks = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-    # emp_tasks.dump(["USER_ID":emp_id, u_name, done, task])
-    '''
