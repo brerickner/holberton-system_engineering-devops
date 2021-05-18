587e78cea280b4f4ea273f516003f7576332b152
brerickner <breannarickner@gmail.com>
2021-05-18 15:31:26 +0000

    adding task 0. pepfixed and output looks good!

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module that queries Reddit API """
+
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """ Method that returns the number of subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {"User-Agent": "change username"}
+    try:
+        response = requests.get(url, headers=headers).json()
+        meow = response.get('data')
+        return (meow['subscribers'])
+    except:
+        return 0
+
+'''if __name__ == '__main__':
+    sub_reddit = argv[1]
+    number_of_subscribers(sub_reddit)'''
